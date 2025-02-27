e294efd82ded90767208fcd0198fd82d0f4fd140
Khali <khalilmakhloufi26@gmail.com>
2025-02-27 12:37:38 +0100

    ex1.5 ex1.6

--- part_1/ex1.6-1.14/src/App.jsx
+++ part_1/ex1.6-1.14/src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Statistics = (props) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <p> {props.type} {props.value}</p>
+  )
+}
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick = {() =>  setGood(good+1)}>Good</button>
+      <button onClick = {() =>  setNeutral(neutral+1)} >Neutral</button>
+      <button onClick = {() =>  setBad(bad+1)} text = "bad">Bad</button>
+
+      <Statistics type = "Good" value = {good}/>
+      <Statistics type = "Neutral" value = {neutral}/>
+      <Statistics type = "Bad" value = {bad}/>
+    </div>
   )
 }
 
--- part_1/ex_1/src/App.jsx
+++ part_1/ex_1/src/App.jsx
@@ -3,14 +3,15 @@
 
 const Header = (props) => {
   return(
-  <h1> {props.coursename} </h1>
+  <h1> {props.course.name} </h1>
   )
 }
 
 const Total = (props) => {
   console.log(props)
+  const parts = props.course.parts
   return  (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 
 }
@@ -23,30 +24,40 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  const parts = props.course.parts
   return (<>
-      <Part part = {props.part1} exercises = {props.exercises1} />
-      <Part part = {props.part2} exercises = {props.exercises2} />
-      <Part part = {props.part3} exercises = {props.exercises3} />
+      <Part part = {parts[0].name} exercises = {parts[0].exercises} />
+      <Part part = {parts[1].name} exercises = {parts[1].exercises} />
+      <Part part = {parts[2].name} exercises = {parts[2].exercises} />
       </>
   )
 }
 
 const App = () => {
 
-const course = 'Half Stack application development'
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
   
-const part3 = 'State of a component'
-const exercises3 = 14
-  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <>
-      <Header coursename = {course} />
-      <Content exercises1= {exercises1} part1={part1} exercises2={exercises2} part2={part2} exercises3={exercises3} part3={part3}/>
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Header course = {course} />
+      <Content course= {course}/>
+      <Total course = {course} />
     </>
   )
 }
