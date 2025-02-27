40cabc4fb6717cde18c674ba9163fc4ec4281adc
Khali <khalilmakhloufi26@gmail.com>
2025-02-27 17:03:46 +0100

    1.10 -1.11

--- part_1/ex1.6-1.14/src/App.jsx
+++ part_1/ex1.6-1.14/src/App.jsx
@@ -3,71 +3,76 @@ import { useState } from 'react'
 
 const Statistic = (props) => {
   return (
-    <p> {props.type} {props.value}</p>
+    <tr><td>{props.type}</td><td>{props.value}</td></tr>
   )
 }
 const Statistics = (props) => {
-  if(props.availible){
+  if (props.availible) {
     return (<p>No feedback given</p>)
   }
   return (
-    <div>
-      {props.types.map((type_name,index) => (
-        <Statistic type = {type_name} value = {props.values[index]}/>  
-      ))}
-    </div>
+    <table>
+      <tbody>
+        {props.types.map((type_name, index) => (
+          <Statistic key ={type_name} type={type_name} value={props.values[index]} />
+        ))}
+      </tbody>
+    </table>
   )
+
+  
 }
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total,setTotal]= useState(0)
-  const [average,setAverage]= useState(0)
-  const [positiveFb,setPositiveFb]= useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positiveFb, setPositiveFb] = useState(0)
 
-  const types=[good,neutral,bad,total,average,positiveFb]
-  const type_names= ["good","neutral","bad","total","average","positiveFb"]
+  const types = [good, neutral, bad, total, average, positiveFb]
+  const type_names = ["good", "neutral", "bad", "total", "average", "positiveFb"]
 
-  const handleClick = (type) => () =>{
+  const handleClick = (type) => () => {
     let updatedGood = good
     let updatedNeutral = neutral
     let updatedBad = bad
 
-    switch(type){
+    switch (type) {
       case "Good":
-        updatedGood =         updatedGood + 1
+        updatedGood = updatedGood + 1
         setGood(updatedGood)
         break
       case "Neutral":
-        updatedNeutral =         updatedNeutral + 1
+        updatedNeutral = updatedNeutral + 1
         setNeutral(updatedNeutral)
         break
       case "Bad":
-        updatedBad=         updatedBad + 1
+        updatedBad = updatedBad + 1
         setBad(updatedBad)
         break
     }
-    const updatedTotal = total+1
+    const updatedTotal = total + 1
     setTotal(updatedTotal)
-    setAverage(getPercentage(updatedGood-updatedBad,updatedTotal))
-    setPositiveFb(getPercentage(updatedGood,updatedTotal))
+    setAverage(getPercentage(updatedGood - updatedBad, updatedTotal))
+    setPositiveFb(getPercentage(updatedGood, updatedTotal))
   }
 
 
-  const getPercentage = (value,total) => () => {
-    return total !=0 ?value/ total:0
+  const getPercentage = (value, total) => () => {
+    return total != 0 ? value / total : 0
   }
 
-  
+
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick = {handleClick("Good")}>Good</button>
-      <button onClick = {handleClick("Neutral")}>Neutral</button>
-      <button onClick = {handleClick("Bad")} >Bad</button>
-      <Statistics availible={good+bad+neutral ==0} types = {type_names} values = {types}/>
+      <button onClick={handleClick("Good")}>Good</button>
+      <button onClick={handleClick("Neutral")}>Neutral</button>
+      <button onClick={handleClick("Bad")} >Bad</button>
+      <h1>Statistics</h1>
+      <Statistics availible={good + bad + neutral == 0} types={type_names} values={types} />
 
     </div>
   )
