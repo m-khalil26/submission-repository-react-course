be223a9b3956a2af3b9ac66ce22f8e364fc2d8f4
Khali <khalilmakhloufi26@gmail.com>
2025-02-27 16:37:11 +0100

    ex1.8

--- part_1/ex1.6-1.14/src/App.jsx
+++ part_1/ex1.6-1.14/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 
 
-const Statistics = (props) => {
+const Statistic = (props) => {
   return (
     <p> {props.type} {props.value}</p>
   )
 }
+const Statistics = (props) => {
+  return (
+    <div>
+      {props.types.map((type_name,index) => (
+        <Statistic type = {type_name} value = {props.values[index]}/>  
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -14,8 +24,10 @@ const App = () => {
   const [average,setAverage]= useState(0)
   const [positiveFb,setPositiveFb]= useState(0)
 
+  const types=[good,neutral,bad,total,average,positiveFb]
+  const type_names= ["good","neutral","bad","total","average","positiveFb"]
+
   const handleClick = (type) => () =>{
-    console.log("type clicked ", {type})
     let updatedGood = good
     let updatedNeutral = neutral
     let updatedBad = bad
@@ -31,18 +43,11 @@ const App = () => {
         break
       case "Bad":
         updatedBad=         updatedBad + 1
-
         setBad(updatedBad)
         break
     }
-    console.log("total", {total})
     const updatedTotal = total+1
     setTotal(updatedTotal)
-    console.log("total after ", {updatedTotal})
-    console.log("goodbefore", {good})
-    console.log("good after",{updatedGood})
-    console.log("bad", {bad})
-
     setAverage(getPercentage(updatedGood-updatedBad,updatedTotal))
     setPositiveFb(getPercentage(updatedGood,updatedTotal))
   }
@@ -59,13 +64,7 @@ const App = () => {
       <button onClick = {handleClick("Good")}>Good</button>
       <button onClick = {handleClick("Neutral")}>Neutral</button>
       <button onClick = {handleClick("Bad")} >Bad</button>
-
-      <Statistics type = "Good" value = {good}/>
-      <Statistics type = "Neutral" value = {neutral}/>
-      <Statistics type = "Bad" value = {bad}/>
-      <Statistics type = "Total" value = {total}/>
-      <Statistics type = "Average" value = {average}/>
-      <Statistics type = "Positive" value = {positiveFb}/>
+      <Statistics types = {type_names} values = {types}/>
 
     </div>
   )
