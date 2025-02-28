bbc72f38e7c6036c6d67e45004a4c53cbf0adcd7
Khali <khalilmakhloufi26@gmail.com>
2025-03-01 00:56:00 +0100

    eex2.4

--- part_2/courseinfo/src/App.jsx
+++ part_2/courseinfo/src/App.jsx
@@ -31,50 +31,65 @@ const Course = (props) => {
 
 const Total = (props) => {
   const parts = props.course.parts
-  const total = parts.reduce((somme,parts)=> {return somme+parts.exercises},0)
+  const total = parts.reduce((somme, parts) => { return somme + parts.exercises }, 0)
   return (<p>Total of {total} exercices</p>)
 
 }
 
+const Courses = (props) => {
+  return props.courses.map((course) => <div key = {course.id} ><Course course={course} /><Total course={course} /></div>)
+}
+
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-      ,
-      {
-        name: 'Redux',
-        exercises: 4,
-        id: 4
-      }
-      ,
-      {
-        name: 'State of a component 2 ',
-        exercises: 5,
-        id: 5
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
-      <Total course={course} />
+      <Courses courses={courses} />
+
     </div>
   )
 }
