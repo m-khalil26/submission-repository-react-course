8521611517174497ba59e13d1e0f48c7ddfd0a14
Khali <khalilmakhloufi26@gmail.com>
2025-03-03 22:49:01 +0100

    ex2.8

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,20 +1,28 @@
+import { event } from '@subsquid/evm-abi'
 import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' , phonenumber:'0769207714'}
   ])
   const [newName, setNewName] = useState('')
 
+  const[newPhonenumber,setNewPhonenumber] = useState('')
+
   const handleSubmit = (event) => {
-    (persons.find(person => person.name===newName))===undefined ? setPersons(persons.concat({ name: newName })) : alert(`${newName} is already taken.`)
+    (persons.find(person => person.name===newName))===undefined ? setPersons(persons.concat({ name: newName ,phonenumber : newPhonenumber})) : alert(`${newName} is already taken.`)
     setNewName('')
+    setNewPhonenumber('')
     event.preventDefault()
   }
 
+
   const handleChange = (event) => {
     setNewName(event.target.value)
+  }
 
+  const handlePhonenumberChange = (event) => {
+    setNewPhonenumber(event.target.value)
   }
   return (
     <div>
@@ -24,11 +32,14 @@ const App = () => {
           name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
+        phonenumber: <input value={newPhonenumber} onChange={handlePhonenumberChange} />
+        </div>
+        <div>
           <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>  {persons.map((person) => <p key={person.name.concat(Math.random(2*5))}>{person.name}</p>)}</div>
+      <div>{persons.map((person) => <p key={person.name.concat(Math.random(2*5))}>{person.name}  {person.phonenumber}</p>)}</div>
 
     </div>
   )
